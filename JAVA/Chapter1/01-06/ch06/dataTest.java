c20273e3a2f180d929e0821731b545940363bd7b
리버덕 <48741014+vivalahm@users.noreply.github.com>
2021-03-01 14:36:26 +0900

    Add files via upload

---
+++ JAVA/Chapter1/01-06/ch06/dataTest.java
@@ -0,0 +1,19 @@
+package ch06;
+
+public class dataTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bnum = 127;
+		//int num =12345678900;
+		//long lnum=12345678900;
+		long lNumber=12345678900L;
+		
+		
+		System.out.println(bnum);
+
+		System.out.println(lNumber);
+		
+	}
+
+}
