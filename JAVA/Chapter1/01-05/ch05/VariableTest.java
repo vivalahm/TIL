0fa751002f778d24a0244b169bd1fb98b7e654aa
리버덕 <48741014+vivalahm@users.noreply.github.com>
2021-03-01 14:36:04 +0900

    Add files via upload

---
+++ JAVA/Chapter1/01-05/ch05/VariableTest.java
@@ -0,0 +1,18 @@
+package ch05;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age, count;
+		age = 10;
+
+		
+		int level = 10000;
+		
+		System.out.println(age);
+		System.out.println(level);
+
+	}
+
+}
