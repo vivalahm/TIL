b952f935f59bb4bcecd643e8c59c66930bc5efcb
리버덕 <48741014+vivalahm@users.noreply.github.com>
2021-03-01 14:35:10 +0900

    Add files via upload

---
+++ JAVA/Chapter1/01-01/ch01/HelloJava.java
@@ -0,0 +1,14 @@
+package ch01; 
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello, HAHA WORLD");
+	
+
+	}
+
+}
